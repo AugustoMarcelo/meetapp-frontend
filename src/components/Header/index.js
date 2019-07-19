d27c6d0d93b275cca507a9b6ff391a3c1a9a57ef
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-18 23:22:49 -0300

    Implementing application logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import { signOut } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+    const profile = useSelector(state => state.user.profile);
+
+    const dispatch = useDispatch();
+
+    function handleSignOut() {
+        dispatch(signOut());
+    }
+
     return (
         <Container>
             <Content>
@@ -18,10 +29,12 @@ export default function Header() {
                 <aside>
                     <Profile>
                         <div>
-                            <strong>Marcelo Augusto</strong>
+                            <strong>{profile.name}</strong>
                             <Link to="/profile">My profile</Link>
                         </div>
-                        <button type="button">Logout</button>
+                        <button type="button" onClick={handleSignOut}>
+                            Logout
+                        </button>
                     </Profile>
                 </aside>
             </Content>
